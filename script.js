a4ce99561812254348e08fbfa3577a4989674efb
[Karol Bobrowski] <[bobrowski.karol009@gmail.com]>
2019-06-06 12:48:09 +0200

    basic popup

--- script.js
+++ script.js
@@ -13,4 +13,37 @@ const map = new mapboxgl.Map({
     // Centrowanie i zoom odpowiada za ustawienie perseptywy w której wczytuje się mapa
   center: [16.939,52.408],
   zoom: 15.50
+
 });
+
+// When a click event occurs on a feature in the projekt-piwny-1ixmu5 layer, open a popup at the
+// location of the feature, with description HTML from its properties.
+map.on('click', 'projekt-piwny-1ixmu5', function (e) {
+var coordinates = e.features[0].geometry.coordinates.slice();
+var description = "text";
+  
+// Ensure that if the map is zoomed out such that multiple
+// copies of the feature are visible, the popup appears
+// over the copy being pointed to.
+while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+}
+  
+new mapboxgl.Popup()
+.setLngLat(coordinates)
+.setHTML(description)
+.addTo(map);
+});
+
+// Change the cursor to a pointer when the mouse is over the projekt-piwny-1ixmu5 layer.
+map.on('mouseenter', 'projekt-piwny-1ixmu5', function () {
+map.getCanvas().style.cursor = 'pointer';
+});
+  
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'projekt-piwny-1ixmu5', function () {
+map.getCanvas().style.cursor = '';
+});
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
